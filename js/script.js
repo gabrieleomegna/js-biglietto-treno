c0d0d1be07b09948d92bb45e201ce14b049cad85
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-15 01:07:02 +0100

    Setted a variable that is the standard price, without the discount

--- js/script.js
+++ js/script.js
@@ -6,5 +6,14 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va scritto in pagina in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca. */
 
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 const userDistance = parseInt(prompt('How many kilometers do you want to travel?'));
 const userAge = parseInt(prompt('How old are you'));
+
+console.log ('The distance is:', userDistance);
+console.log ('Your age is:', userAge);
+
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let priceStandard = 0.21 * userDistance;
+console.log ('Price standard is:',priceStandard,'€');
